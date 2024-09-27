76a7efbd87ed88b92771be8a175173c31c94e10d
Jie-jiao05 <jiejiao404@gmail.com>
2024-09-27 13:45:20 -0400

    Find sth missed

--- starter_folder-main/scripts/03-test_data.R
+++ starter_folder-main/scripts/03-test_data.R
@@ -12,10 +12,24 @@
 library(tidyverse)
 
 #### Test data ####
-data <- read_csv("starter_folder-main/data/raw_data/simulated.csv")
+data <- read_csv("~/Paper 1/starter_folder-main/data/raw_data/simulated.csv")
 
 # Test for negative numbers
 data$number_of_death |> min() <= 0
 
 # Test for NAs
 all(is.na(data$number_of_death))
+
+# Test if each CIVIC_CENTRE is within the valid civic centers
+check_civic_centres <- function(data) {
+  all(data$CIVIC_CENTRE %in% valid_civic_centres)
+}
+is_valid <- check_civic_centres(data)
+print(is_valid)
+
+# Function to check if PLACE_OF_DEATH values are valid
+check_places_of_death <- function(data) {
+  all(data$PLACE_OF_DEATH %in% valid_places_of_death)
+}
+are_places_valid <- check_places_of_death(data)
+print(are_places_valid)
